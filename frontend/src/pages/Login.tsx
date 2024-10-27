7e5bf72a098d7a7b37cc44e4ca9c769bc33f779b
Tate, Thane <thanetate@my.unt.edu>
2024-10-26 21:49:51 -0500

    signup

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,36 @@
-
+import Header from "../components/Header/page";
+import { Link } from "react-router-dom";
+import '../styles/login.css';
 
 function Login () {
     return (
         <>
-            <h1>Login Page</h1>
+            <Header />
+            {/* onsubmit here */}
+			<form className="login-form">
+				<label className="login-label">Email</label>
+				<input
+					className="login-input-email"
+					type="email"
+					placeholder="enter email..."
+					// value={data.email}
+					// onChange={(e) => setData({ ...data, email: e.target.value })}
+				/>
+				<label className="login-label">Password</label>
+				<input
+					className="login-input-password"
+					type="password"
+					placeholder="enter password..."
+					// value={data.password}
+					// onChange={(e) => setData({ ...data, password: e.target.value })}
+				/>
+				<button className="login-button" type="submit">
+					Login
+				</button>
+				<Link to="/signup" className="signup-link">
+					Sign up
+				</Link>
+			</form>
         </>
     );
 };
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,40 @@
+import Header from "../components/Header/page";
+
 
 function Signup () {
     return (
         <>
-            <h1>Sign up page</h1>
+            <Header />
+            {/* Register user on submit */}
+			<form className="register-form">
+				<label className="register-label">Name</label>
+				<input
+					className="register-input-name"
+					type="text"
+					placeholder="enter name..."
+					// value={data.name}
+					// onChange={(e) => setData({ ...data, name: e.target.value })}
+				/>
+				<label className="register-label">Email</label>
+				<input
+					className="register-input-email"
+					type="email"
+					placeholder="enter email..."
+					// value={data.email}
+					// onChange={(e) => setData({ ...data, email: e.target.value })}
+				/>
+				<label className="register-label">Password</label>
+				<input
+					className="register-input-password"
+					type="password"
+					placeholder="enter password..."
+					// value={data.password}
+					// onChange={(e) => setData({ ...data, password: e.target.value })}
+				/>
+				<button className="register-button" type="submit">
+					Submit
+				</button>
+			</form>
         </>
     );
 };
