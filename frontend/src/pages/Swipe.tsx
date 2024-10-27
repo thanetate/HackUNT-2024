8c048820eed1501adbb781b7509c3509a5af24d9
jcl0251 <josephcleal529@gmail.com>
2024-10-26 22:45:35 -0500

    Swipe

--- frontend/src/pages/Swipe.tsx
+++ frontend/src/pages/Swipe.tsx
@@ -1,15 +1,16 @@
 import TinderCards from "../components/TinderCards/TinderCards";
+import Header from "../components/Header/page";
 
 function Swipe () {
   return (
-    <div style={{
-      
-    }}> 
-        <h1>Swipe</h1>
-        <TinderCards /> {/* Displaying the TinderCards component */}
-    </div>
+    
+        <div style={{
+        }}> 
+            <Header/>
+            <h1>Swipe</h1>
+            <TinderCards /> {/* Displaying the TinderCards component */}
+        </div>
   )
-  
 };
 
 export default Swipe;
