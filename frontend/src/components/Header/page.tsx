5cfd921da052f576549dbec26fec6112d594eb84
Tate, Thane <thanetate@my.unt.edu>
2024-10-26 20:53:53 -0500

    home

---
+++ frontend/src/components/Header/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<>
+			<div className="nav-container">
+				<div className="title">Monster Match</div>
+				<div className="nav-links-container">
+          {/* <Link href="/">
+            <p className="redirects">Home</p>
+          </Link>
+          <div className="bar">|</div>
+          <Link href="/swipe">
+            <p className="redirects">Swipes</p>
+          </Link>
+          <div className="bar">|</div>
+          <Link href="/dashboard">
+            <p className="redirects">Dashboard</p>
+          </Link> */}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Header;
