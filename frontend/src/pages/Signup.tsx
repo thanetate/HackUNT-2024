2a293e68364800a15e6efe774c6a3ccbc566598f
Tate, Thane <thanetate@my.unt.edu>
2024-10-26 23:36:25 -0500

    signup

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,55 +1,81 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../styles/login.css";
+import Header from "../components/Header/page";
 
 function Signup() {
-  // navigate
-  const navigate = useNavigate();
+	// navigate
+	const navigate = useNavigate();
 
-  // default data
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
+	// default data
+	const [data, setData] = useState({
+		email: "",
+		password: "",
+	});
 
-  // function that registers the user to the db
-  const registerUser = async (e: React.FormEvent) => {
-    // prevents default form submit
-    e.preventDefault();
-    // destruct's the email and password
-    const { email, password } = data;
-    try {
-      // sends a POST request to the /signup endpoint
-      const response = await axios.post("http://localhost:3000/signup", {
-        email,
-        password,
-      });
-      const success = response.status === 201;
-      console.log(success);
-      if (success) navigate("/dashboard");
-    } catch (error) {
-      console.error("Error registering user:", error);
-      // Optionally, you can set an error state to display an error message to the user
-    }
-  };
+	// function that registers the user to the db
+	const registerUser = async (e: React.FormEvent) => {
+		// prevents default form submit
+		e.preventDefault();
+		// destruct's the email and password
+		const { email, password } = data;
+		try {
+			// sends a POST request to the /signup endpoint
+			const response = await axios.post("http://localhost:3000/signup", {
+				email,
+				password,
+			});
+			const success = response.status === 201;
+			console.log(success);
+			if (success) navigate("/dashboard");
+		} catch (error) {
+			console.error("Error registering user:", error);
+			// Optionally, you can set an error state to display an error message to the user
+		}
+	};
 
-  return (
-    <form onSubmit={registerUser}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={data.email}
-        onChange={(e) => setData({ ...data, email: e.target.value })}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={data.password}
-        onChange={(e) => setData({ ...data, password: e.target.value })}
-      />
-      <button type="submit">Sign Up</button>
-    </form>
-  );
+	return (
+		// <form onSubmit={registerUser}>
+		//   <input
+		//     type="email"
+		//     placeholder="Email"
+		//     value={data.email}
+		//     onChange={(e) => setData({ ...data, email: e.target.value })}
+		//   />
+		//   <input
+		//     type="password"
+		//     placeholder="Password"
+		//     value={data.password}
+		//     onChange={(e) => setData({ ...data, password: e.target.value })}
+		//   />
+		//   <button type="submit">Sign Up</button>
+		// </form>
+		<>
+			<Header />
+			<form className="register-form" onSubmit={registerUser}>
+				<label className="register-label">Email</label>
+				<input
+					className="register-input-email"
+					type="email"
+					placeholder="enter email..."
+					value={data.email}
+					onChange={(e) => setData({ ...data, email: e.target.value })}
+				/>
+				<label className="register-label">Password</label>
+				<input
+					className="register-input-password"
+					type="password"
+					placeholder="Password"
+					value={data.password}
+					onChange={(e) => setData({ ...data, password: e.target.value })}
+				/>
+				<button className="register-button" type="submit">
+					Submit
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default Signup;
